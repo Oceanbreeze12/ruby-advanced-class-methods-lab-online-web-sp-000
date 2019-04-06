ea7d29f9760ec568f70d91e19510463ef01878f5
darling-rockstar-4852 <daisyrene483@gmail.com>
2019-04-06 17:25:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,62 @@ class Song
     @@all
   end
 
-  def save
-    self.class.all << self
+  
+	  
+def self.create
+    song = self.new
+    song.save
+    song
   end
 
-end
+   def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name  
+    song  
+  end 
+
+   def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song 
+  end
+
+   def self.find_by_name(song_name)
+    self.all.detect { |song| song.name == song_name }
+  end
+
+   def self.find_or_create_by_name(song_name)
+    find_by_name(song_name) || create_by_name(song_name)
+  end
+
+   def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+
+   def self.new_from_filename(filename)
+    title = filename.split(" - ")
+    artist = title[0]
+    song_name, extension = title[1].split(".")
+
+     song = self.new 
+    song.artist_name = artist 
+    song.name = song_name 
+    song 
+  end
+
+   def self.create_from_filename(filename)
+    title = filename.split(" - ")
+    artist = title[0]
+    song_name, extension = title[1].split(".")
+
+     song = self.create 
+    song.artist_name = artist 
+    song.name = song_name 
+    song 
+  end
+end	
+
+
+
+
+
